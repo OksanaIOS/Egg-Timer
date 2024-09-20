650d09851eded16b6cbfea150af2cffe6ad57e21
Oksana ogay <example@email.com>
2024-09-20 13:51:20 +0500

    add button and set constraints for buttons

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -45,6 +45,11 @@ class ViewController: UIViewController {
     private lazy var hardImageView = UIImageView.makeEggImageView(named: "hard_egg")
     
 
+    private lazy var softButton = UIButton.makeButton(title: "Soft", target: self, action: #selector(eggsButtonTupped))
+    private lazy var mediumButton = UIButton.makeButton(title: "Medium", target: self, action: #selector(eggsButtonTupped))
+    private lazy var hardButton = UIButton.makeButton(title: "Hard", target: self, action: #selector(eggsButtonTupped))
+    
+
     
     
     private lazy var timerView: UIView  = {
@@ -76,6 +81,9 @@ class ViewController: UIViewController {
         
         
     }
+    @objc func eggsButtonTupped(_ sender: UIButton) {
+        print(sender.currentTitle)
+    }
 }
 //MARK: - Set Views and Set Constraints
     
@@ -91,6 +99,10 @@ extension ViewController {
         eggsStackView.addArrangedSubview(mediumImageView)
         eggsStackView.addArrangedSubview(hardImageView)
         
+        softImageView.addSubview(softButton)
+        mediumImageView.addSubview(mediumButton)
+        hardImageView.addSubview(hardButton)
+        
         
     }
     private func setConstraints() {
@@ -102,7 +114,23 @@ extension ViewController {
             
             progressView.centerYAnchor.constraint(equalTo: timerView.centerYAnchor),
             progressView.leadingAnchor.constraint(equalTo: timerView.leadingAnchor),
-            progressView.trailingAnchor.constraint(equalTo: timerView.trailingAnchor)
+            progressView.trailingAnchor.constraint(equalTo: timerView.trailingAnchor),
+            
+            softButton.topAnchor.constraint(equalTo: softImageView.topAnchor),
+            softButton.leadingAnchor.constraint(equalTo: softImageView.leadingAnchor),
+            softButton.bottomAnchor.constraint(equalTo: softImageView.bottomAnchor),
+            softButton.trailingAnchor.constraint(equalTo: softImageView.trailingAnchor),
+            
+            
+            mediumButton.topAnchor.constraint(equalTo: mediumImageView.topAnchor),
+            mediumButton.leadingAnchor.constraint(equalTo: mediumImageView.leadingAnchor),
+            mediumButton.bottomAnchor.constraint(equalTo: mediumImageView.bottomAnchor),
+            mediumButton.trailingAnchor.constraint(equalTo: mediumImageView.trailingAnchor),
+            
+            hardButton.topAnchor.constraint(equalTo: hardImageView.topAnchor),
+            hardButton.leadingAnchor.constraint(equalTo: hardImageView.leadingAnchor),
+            hardButton.bottomAnchor.constraint(equalTo: hardImageView.bottomAnchor),
+            hardButton.trailingAnchor.constraint(equalTo: hardImageView.trailingAnchor)
             
         ])
         
@@ -117,12 +145,23 @@ extension ViewController {
         static func makeEggImageView(named imageName: String) -> UIImageView {
         let imageView = UIImageView(image: UIImage(named: imageName))
         imageView.contentMode = .scaleAspectFit
+        imageView.isUserInteractionEnabled = true
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }
 }
         
-         
+private extension UIButton {
+    static func makeButton(title: String, target: Any?, action: Selector) -> UIButton {
+        let button = UIButton()
+        button.setTitle(title, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .black)
+        button.tintColor = .white
+        button.addTarget(target, action:action, for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }
+}
         
     
 #Preview {
